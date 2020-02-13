c3199b4cd125440f71ac8cfd2d20349f9136fca7
adminl <adminl@mambo28.gtming.local>
2020-02-13 16:32:22 +0100

    premier commit sur brancheclement

--- test/Test1_1.java
+++ test/Test1_1.java
@@ -2,4 +2,7 @@ package test;
 
 public class Test1_1 {
 
+	 private String s = "la la lalalala hehehe la la la";
+	 
+	 private String d = "clement 1";
 }
--- test2/Test2_2.java
+++ test2/Test2_2.java
@@ -2,4 +2,5 @@ package test2;
 
 public class Test2_2 {
 
+	private String s5 = "en vrai Trump n'est pas si mauvais";
 }
