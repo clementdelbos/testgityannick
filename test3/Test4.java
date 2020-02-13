37563e347fe54a11e6fd79c808d036d101c1ebb4
adminl <adminl@mambo28.gtming.local>
2020-02-13 16:40:48 +0100

    deuxieme commit branche clement

---
+++ test3/Test4.java
@@ -0,0 +1,6 @@
+package test3;
+
+public class Test4 {
+	private String s5 = "en vrai Trump n'est pas si mauvais";
+
+}
---
+++ test3/test3.java
@@ -0,0 +1,6 @@
+package test3;
+
+public class test3 {
+	private String s5 = "en vrai Trump n'est pas si mauvais";
+
+}
